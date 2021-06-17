412716b9bc734eac791244fa2a55b787f65d2581
Dariane Abich <darianeabich@gmail.com>
2021-06-17 11:42:44 -0300

    Implementação dos testes e inclusão das funções fakeAsync() e tick() em conformidade com o tempo passado no debounce

--- src/app/shared/components/photo-frame/photo-frame.component.spec.ts
+++ src/app/shared/components/photo-frame/photo-frame.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { PhotoFrameComponent } from './photo-frame.component';
 import { PhotoFrameModule } from './photo-frame.module';
 
@@ -18,4 +18,29 @@ describe(PhotoFrameComponent.name, () => {
   it('Should create component', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`#${PhotoFrameComponent.prototype.like.name}
+    should trigger (@Output liked) once
+    when called multiple times within debounce time`, fakeAsync(() => {
+      fixture.detectChanges(); // disparado o ciclo de vida
+      let times = 0; // times iniciado em zero
+      component.liked.subscribe(() => times++); // toda a vez que chamar like(), times soma 1
+      component.like(); // vai chamar o like 2x, mas chegará a contabilizar os dois devido a rapidez
+      component.like();
+      tick(500); // vai pausar o teste por 500ms para que possa processar o like
+      expect(times).toBe(1);
+  }));
+
+  it(`#${PhotoFrameComponent.prototype.like.name}
+    should trigger (@Output liked) two times
+    when called outside debounce time`, fakeAsync(() => {
+      fixture.detectChanges();
+      let times = 0;
+      component.liked.subscribe(() => times++);
+      component.like();
+      tick(500);
+      component.like();
+      tick(500);
+      expect(times).toBe(2);
+    }))
 });
