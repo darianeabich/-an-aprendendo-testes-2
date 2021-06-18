e0e85028e670fb311b43b08c20c9d4fbaabf6b70
Dariane Abich <darianeabich@gmail.com>
2021-06-18 11:34:41 -0300

    Criação do component photoboard e seu template

---
+++ src/app/shared/components/photo-board/photo-board.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { PhotoFrameModule } from '../photo-frame/photo-frame.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PhotoFrameModule,
+  ],
+  exports: []
+})
+export class PhotoBoardModule {}
