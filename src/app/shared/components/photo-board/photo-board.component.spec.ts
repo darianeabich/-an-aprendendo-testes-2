ad6608eaf34f9c00de6aec5e3589e25bc12e0d8c
Dariane Abich <darianeabich@gmail.com>
2021-06-18 14:34:05 -0300

    Trabalhando com ngOnChange nos testes

--- src/app/shared/components/photo-board/photo-board.component.spec.ts
+++ src/app/shared/components/photo-board/photo-board.component.spec.ts
@@ -1,6 +1,21 @@
+import { SimpleChange, SimpleChanges } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Photo } from './interfaces/photo';
 import { PhotoBoardComponent } from './photo-board.component';
 import { PhotoBoardModule } from './photo-board.module';
+
+// criação de uma função utilitária p/ criar os dados do teste
+function buildPhotosList(): Photo[] {
+  const photos: Photo[] = [];
+  for (let i = 0; i < 8; i++) {
+    photos.push({
+      id: i + 1,
+      url: '',
+      description: ''
+    });
+  }
+  return photos;
+}
 
 describe(PhotoBoardComponent.name, () => {
   let fixture: ComponentFixture<PhotoBoardComponent>;
@@ -13,5 +28,23 @@ describe(PhotoBoardComponent.name, () => {
 
     fixture = TestBed.createComponent(PhotoBoardComponent);
     component = fixture.componentInstance;
+  });
+
+  it(`Should display rows and columns when (@Output photos) has value`, () => {
+    component.photos = buildPhotosList();
+    fixture.detectChanges();
+    const change: SimpleChanges = {
+      photos: new SimpleChange([], component.photos, true)
+    }
+    component.ngOnChanges(change);
+    expect(component.rows.length)
+      .withContext('Number of rows')
+      .toBe(2);
+    expect(component.rows[0].length)
+      .withContext('Number of columns from the first row')
+      .toBe(4);
+    expect(component.rows[1].length)
+      .withContext('Number of columns from the second row')
+      .toBe(4);
   });
 });
